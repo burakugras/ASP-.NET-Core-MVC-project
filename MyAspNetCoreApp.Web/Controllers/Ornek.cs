076f0d334ea2d7461776c76632ba38c43c00d905
burak.ugras <burakugras67@gmail.com>
2023-01-16 22:43:50 +0300

    Modify 101

---
+++ MyAspNetCoreApp.Web/Controllers/Ornek.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class Product2
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Ornek : Controller
+    {
+        public IActionResult Index()
+        {
+            var productList = new List<Product2>()
+            {
+                new(){Id=1,Name="Kalem"},
+                new(){Id=2,Name="Defter"},
+                new(){Id=3,Name="Silgi"}
+            };
+
+            return View(productList);
+        }
+
+        public IActionResult Index2() 
+        {
+            var surName = TempData["name"];
+            return View();
+        }
+
+        public IActionResult Index3()
+        {
+            //veri tabanına kaydetme işlemleri
+            return RedirectToAction("Index","Ornek");
+
+        }
+
+        public IActionResult ParametreView(int id) 
+        {
+
+            return RedirectToAction("JsonResultParametre",new {id=id});
+        }
+
+        public IActionResult JSonResultParametre(int id)
+        {
+            return Json(new { id = id });
+        }
+
+        public IActionResult ContentResult()
+        {
+            return Content("ContentResult");
+        }
+        
+        public IActionResult JSonResult() 
+        { 
+            return Json(new {id= 1,name="kalem",price=100});    
+        }
+
+        public IActionResult EmptyResult() 
+        {
+            return new EmptyResult();
+        }
+    }
+}
---
+++ MyAspNetCoreApp.Web/Models/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace MyAspNetCoreApp.Web.Models
+{
+    public class ProductRepository
+    {
+        private static List<Product> _products = new List<Product>()
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 1999, Stock = 100 },
+            new Product { Id = 2, Name = "Mouse", Price = 99, Stock = 150 },
+            new Product { Id = 3, Name = "Keyboard", Price = 299, Stock = 120 }
+        };
+        public List<Product> GetAll() => _products;
+
+        public void Add(Product newProduct)=>_products.Add(newProduct);
+
+        public void Remove(int id)
+        {
+            var hasProduct=_products.FirstOrDefault(x => x.Id == id);
+            if (hasProduct==null)
+            {
+                throw new Exception($"Bu id ({id})'ye sahip ürün bulunmamaktadır.");
+            }
+
+            _products.Remove(hasProduct);
+        }
+
+        public void Update(Product updateProduct)
+        {
+            var hasProduct = _products.FirstOrDefault(x => x.Id == updateProduct.Id);
+            if (hasProduct == null)
+            {
+                throw new Exception($"Bu id ({updateProduct.Id})'ye sahip ürün bulunmamaktadır.");
+            }
+
+            hasProduct.Name = updateProduct.Name;
+            hasProduct.Price = updateProduct.Price;
+            hasProduct.Stock=updateProduct.Stock;
+
+            var index=_products.FindIndex(x=>x.Id == updateProduct.Id);
+            _products[index] = hasProduct;
+        }
+    }
+}
