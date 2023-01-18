96122d9721f2aadc43cf2647a983172fa20a76ad
burak.ugras <burakugras67@gmail.com>
2023-01-18 22:40:41 +0300

    db first applied

--- MyAspNetCoreApp.Web/Controllers/ProductsController.cs
+++ MyAspNetCoreApp.Web/Controllers/ProductsController.cs
@@ -5,26 +5,38 @@ namespace MyAspNetCoreApp.Web.Controllers
 {
     public class ProductsController : Controller
     {
+        private AppDbContext _context;
         private readonly ProductRepository _productRepository;
-        public ProductsController()
+        public ProductsController(AppDbContext context)
         {
             _productRepository= new ProductRepository();
 
-            if (!_productRepository.GetAll().Any())
+            _context = context;
+
+            if (!_context.Products.Any())
             {
-                
+                _context.Products.Add(new Product { Name = "Kalem 1", Price = 10, Stock = 100, Color = "Red", Height = 10,Width=20 });
+                _context.Products.Add(new Product { Name = "Kalem 2", Price = 15, Stock = 200, Color = "Blue", Height = 10, Width = 20 });
+                _context.Products.Add(new Product { Name = "Kalem 3", Price = 20, Stock = 300, Color = "Purple", Height = 10, Width = 20 });
+
+                _context.SaveChanges();
             }
+            
 
         }
         public IActionResult Index()
         {
-            var products = _productRepository.GetAll();
+            var products = _context.Products.ToList();
             return View(products);
         }
 
         public IActionResult Remove(int id) 
         {
-            _productRepository.Remove(id);
+            var product = _context.Products.Find(id);
+            _context.Products.Remove(product);
+
+            _context.SaveChanges();
+
             return RedirectToAction("Index");
         }
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ASP-.NET-Core-MVC-project
 basic .NET Core MVC project 101 
+# DB first applied
