58b38876870440e7dc1944b0e048e511b2d495e3
burak.ugras <burakugras67@gmail.com>
2023-01-20 21:02:47 +0300

    Type safe method using added to ProductsController Class

--- MyAspNetCoreApp.Web/Controllers/ProductsController.cs
+++ MyAspNetCoreApp.Web/Controllers/ProductsController.cs
@@ -47,7 +47,7 @@ namespace MyAspNetCoreApp.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Add(string Name,decimal Price,int Stock,string Color)
+        public IActionResult Add(Product newProduct)
         {
             //Request Header-Body
 
@@ -58,7 +58,8 @@ namespace MyAspNetCoreApp.Web.Controllers
             //var stock= int.Parse(HttpContext.Request.Form["Stock"].ToString());
             //var color= HttpContext.Request.Form["Color"].ToString();
 
-            Product newProduct=new Product() { Name=Name,Price=Price, Color=Color, Stock = Stock };
+            //2. Yöntem
+            //Product newProduct=new Product() { Name=Name,Price=Price, Color=Color, Stock = Stock };
             
             _context.Products.Add(newProduct);
             _context.SaveChanges();
