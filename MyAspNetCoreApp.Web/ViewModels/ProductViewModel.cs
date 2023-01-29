8fad0f8a9f073107d0d4497ab2caf39487ad6362
burak.ugras <burakugras67@gmail.com>
2023-01-29 22:22:42 +0300

    Model validation codes are improved

--- MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
+++ MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
@@ -12,11 +12,14 @@ namespace MyAspNetCoreApp.Web.ViewModels
         public decimal? Price { get; set; }
         [Required(ErrorMessage = "Stok alanı boş bırakılamaz")]
         public int? Stock { get; set; }
+        [Required(ErrorMessage = "Renk boş bırakılamaz")]
         public string? Color { get; set; }
         public bool IsPublish { get; set; }
-        public int Expire { get; set; }
+        [Required(ErrorMessage = "Süre boş bırakılamaz")]
+        public int? Expire { get; set; }
+        [Required(ErrorMessage = "Yayınlanma tarihi boş olamaz")]
         public DateTime? PublishDate { get; set; }
-        [Required(ErrorMessage = "Açıklama alanı boş bırakılamaz")]
+        [Required(ErrorMessage = "Açıklama boş bırakılamaz")]
         public string Description { get; set; }
     }
 }
