435741daeabab970c4d831544062d58a2a2b9ad9
burak.ugras <burakugras67@gmail.com>
2023-01-29 22:13:15 +0300

    Model Validation codes are added

--- MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
+++ MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
@@ -1,19 +1,22 @@
-﻿using Microsoft.Build.Framework;
+﻿
+using System.ComponentModel.DataAnnotations;
 
 namespace MyAspNetCoreApp.Web.ViewModels
 {
     public class ProductViewModel
     {
         public int Id { get; set; }
-
-        [Required]
+        [Required(ErrorMessage = "İsim alanı boş bırakılamaz")]
         public string? Name { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
+        [Required(ErrorMessage ="Fiyat alanı boş bırakılamaz")]    
+        public decimal? Price { get; set; }
+        [Required(ErrorMessage = "Stok alanı boş bırakılamaz")]
+        public int? Stock { get; set; }
         public string? Color { get; set; }
         public bool IsPublish { get; set; }
         public int Expire { get; set; }
         public DateTime? PublishDate { get; set; }
+        [Required(ErrorMessage = "Açıklama alanı boş bırakılamaz")]
         public string Description { get; set; }
     }
 }
