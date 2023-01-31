47619414594496a09fced3d286c90d2e413d9b0f
burak.ugras <burakugras67@gmail.com>
2023-01-31 17:56:58 +0300

    String Lengths added

--- MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
+++ MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
@@ -6,6 +6,7 @@ namespace MyAspNetCoreApp.Web.ViewModels
     public class ProductViewModel
     {
         public int Id { get; set; }
+        [StringLength(50,ErrorMessage ="İsim alanına en fazla 50 karakter girilebilir.")]
         [Required(ErrorMessage = "İsim alanı boş bırakılamaz")]
         public string? Name { get; set; }
         [Range(1, 1000, ErrorMessage = "Fiyat alanı 1 ile 1000 arasında bir değer olmalıdır.")]
@@ -21,6 +22,7 @@ namespace MyAspNetCoreApp.Web.ViewModels
         public int? Expire { get; set; }
         [Required(ErrorMessage = "Yayınlanma tarihi boş olamaz")]
         public DateTime? PublishDate { get; set; }
+        [StringLength(300, MinimumLength =50, ErrorMessage = "Açıklama alanı 50 ile 300 karakter arasında olabilir.")]
         [Required(ErrorMessage = "Açıklama boş bırakılamaz")]
         public string Description { get; set; }
     }
