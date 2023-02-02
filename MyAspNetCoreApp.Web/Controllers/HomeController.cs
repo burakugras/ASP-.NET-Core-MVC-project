dc4d1ca63e64856a39351fac729314af9ea40a93
burak.ugras <burakugras67@gmail.com>
2023-02-02 19:22:18 +0300

    Partial View Model binding 3

--- MyAspNetCoreApp.Web/Controllers/HomeController.cs
+++ MyAspNetCoreApp.Web/Controllers/HomeController.cs
@@ -13,15 +13,12 @@ namespace MyAspNetCoreApp.Web.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly AppDbContext _context;
 
-        public HomeController(AppDbContext context)
-        {
-            _context = context;
-        }
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
+                
 
         public IActionResult Index()
         {
@@ -42,6 +39,18 @@ namespace MyAspNetCoreApp.Web.Controllers
 
         public IActionResult Privacy()
         {
+            var products = _context.Products.OrderByDescending(x => x.Id).Select(x => new ProductPartialViewModel()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Price = x.Price,
+                Stock = x.Stock
+            }).ToList();
+
+            ViewBag.productListPartialViewModel = new ProductListPartialViewModel()
+            {
+                Products = products
+            };
             return View();
         }
 
