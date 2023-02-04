d3ffb464ec531af9e90656e33cb97c717c5cf625
burak.ugras <burakugras67@gmail.com>
2023-02-04 19:21:33 +0300

    Partial View Model binding 3

--- MyAspNetCoreApp.Web/Program.cs
+++ MyAspNetCoreApp.Web/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
