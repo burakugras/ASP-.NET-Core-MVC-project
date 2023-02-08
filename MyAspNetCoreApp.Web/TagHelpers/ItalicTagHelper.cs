61200f033aa3660674731d2a85c06e09278483c1
burak.ugras <burakugras67@gmail.com>
2023-02-08 23:59:46 +0300

    Custom tag helper added

---
+++ MyAspNetCoreApp.Web/TagHelpers/ItalicTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MyAspNetCoreApp.Web.TagHelpers
+{
+    public class ItalicTagHelper:TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.PreContent.SetHtmlContent("<i>");
+
+            output.PostContent.SetHtmlContent("</li>");
+
+        }
+    }
+}
