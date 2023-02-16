059694fa865a7bca6391faa936a3f42d11ad8b14
burak.ugras <burakugras67@gmail.com>
2023-02-16 19:31:26 +0300

    Coding for GetById page

--- MyAspNetCoreApp.Web/Controllers/ProductsController.cs
+++ MyAspNetCoreApp.Web/Controllers/ProductsController.cs
@@ -26,9 +26,9 @@ namespace MyAspNetCoreApp.Web.Controllers
             return View(_mapper.Map<List<ProductViewModel>>(products));
         }
 
-        public IActionResult GetById(int id)
+        public IActionResult GetById(int productId)
         {
-            var product = _context.Products.Find(id);
+            var product = _context.Products.Find(productId);
             return View(_mapper.Map<ProductViewModel>(product));
         }
 
