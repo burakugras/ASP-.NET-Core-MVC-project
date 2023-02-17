4c7d47f86c523fcbab3129f673186895ed725d2e
burak.ugras <burakugras67@gmail.com>
2023-02-18 02:29:01 +0300

    conventional routing creating with asterix char

--- MyAspNetCoreApp.Web/Controllers/BlogController.cs
+++ MyAspNetCoreApp.Web/Controllers/BlogController.cs
@@ -4,9 +4,13 @@ namespace MyAspNetCoreApp.Web.Controllers
 {
     public class BlogController : Controller
     {
-        public IActionResult Article()
+        //blog/article/makale-ismi/id
+        public IActionResult Article(string name,int id)
         {
+            //var routes = Request.RouteValues["article"];
             return View();
         }
+
+        
     }
 }
